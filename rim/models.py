5aa7f95be181b0657812d548c79cad59987d2e87
Patrick Ayres <ayres732@live.missouristate.edu>
2018-05-08 12:30:44 -0500

    Add some preliminary models

--- rim/models.py
+++ rim/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.utils.timezone import now
 
-# Create your models here.
+class Equipment(models.Model):
+    serial_no = models.CharField(max_length=15)
+    equipment_model = models.CharField()
+    equipment_type = models.ForeignKey(EquipmentType)
+    count = models.IntegerField()
+
+class EquipmentType(models.Model):
+    type_name = models.charField(max_length=30)
+
+class Checkout(models.Model):
+    client = models.CharField(max_length=50)
+    timestamp = models.DateTimeField(default=now)
+    returned_timestamp = models.DateTimeField(null=True)
+    location = models.ForeignKey(Location)
+    comment = models.TextField()
+    equipment = models.ForeignKey(Equipment)
+
+class Location(models.Model):
+    building = models.CharField(max_length=50)
+    room = models.CharField(max_length=50, blank=True)
+
+class Note(models.Model):
