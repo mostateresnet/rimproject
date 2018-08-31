a8f6db1d7631cfac2d9f592b10814d6ee5edb8c1
Patrick Ayres <ayres732@live.missouristate.edu>
2018-08-31 08:13:38 -0500

    Do lots of work on group pages, including an add and edit page

--- rim/urls.py
+++ rim/urls.py
@@ -4,5 +4,7 @@ from rim import views
 
 urlpatterns = [
     path('', views.BaseView.as_view(), name='base'),
-    path('group/', views.GroupView.as_view(), name='group'),
+    path('group/', views.ListGroupView.as_view(), name='group'),
+    path('group/add', views.AddGroupView.as_view(), name='addgroup'),
+    path('group/edit/<int:pk>', views.EditGroupView.as_view(), name='editgroup')
 ]
--- rim/views.py
+++ rim/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.urls import reverse_lazy
-from django.views.generic import CreateView, TemplateView
+from django.views.generic import CreateView, TemplateView, ListView, UpdateView
 
 from rim.models import Group
 from rim.forms import GroupForm
@@ -8,8 +8,25 @@ from rim.forms import GroupForm
 class BaseView(TemplateView):
     template_name='rim/base.html'
 
-class GroupView(CreateView):
+class ListGroupView(ListView):
     template_name='rim/group.html'
+    model = Group
+
+    def get_queryset(self):
+        qset = super(ListGroupView, self).get_queryset()
+        qset = Group.objects.all()
+        return qset
+
+class AddGroupView(CreateView):
+    template_name='rim/add_group.html'
+    model = Group
+    form_class = GroupForm
+    success_url = reverse_lazy('group')
+
+
+
+class EditGroupView(UpdateView):
+    template_name='rim/edit_group.html'
     model = Group
     form_class = GroupForm
     success_url = reverse_lazy('group')
