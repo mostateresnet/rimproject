bc863d051b08d121b7492104d9d98f2c08cd10ad
Austin Stewart <aaaustin10@gmail.com>
2019-01-03 14:48:21 -0600

    Improve creation script performance.

--- populate_rim.py
+++ populate_rim.py
@@ -11,7 +11,6 @@ django.setup()
 from rim.models import Equipment, Checkout, EquipmentType, Location, Client
 
 def populate():
-
     equipment_type = [
         {'type_name':'Laptop'},
         {'type_name':'Tablet'},
@@ -19,93 +18,76 @@ def populate():
     ]
 
     for equip in equipment_type:
-        add_equipment_type(equip["type_name"])
+        # TODO make bulk create
+        EquipmentType.objects.get_or_create(type_name=equip['type_name'])
 
     print("after equipment type")
     # opens the location list file, reads in the json object and loops through it to add the locations
     with open('locationlist.txt') as locationfile:
         location = json.load(locationfile)
+        location_objects = []
         for loc in location:
-            add_location(loc['building'], loc['room'])
-    locationfile.close()
+            location_object = Location(building = loc['building'], room = loc['room'])
+            location_objects.append(location_object)
+        Location.objects.bulk_create(location_objects)
     print("after location")
 
     with open('equipmentlist.txt') as equipmentfile:
         equipment = json.load(equipmentfile)
+        equipment_objects = []
         for i in equipment:
-            add_equipment(i['serial_no'], i['equipment_model'],
-                i['equipment_type'], i['count'], i['manufacturer'],
-                i['service_tag'], i['smsu_tag'], i['cpu'], i['optical_drive'],
-                i['size'], i['memory'], i['other_connectivity'], i['hard_drive'],
-                i['usb_ports'], i['video_card'], i['removable_media'],
-                i['physical_address'], i['purchase_price'], i['purchase_info'])
-    equipmentfile.close()
+            equipment_object = Equipment(
+                serial_no = i['serial_no'],
+                equipment_model = i['equipment_model'],
+                # TODO make all of these get requests at once
+                equipment_type = EquipmentType.objects.get(type_name=i['equipment_type']),
+                count = i['count'],
+                manufacturer = i['manufacturer'],
+                service_tag = i['service_tag'],
+                smsu_tag = i['smsu_tag'],
+                cpu = i['cpu'],
+                optical_drive = i['optical_drive'],
+                size = i['size'],
+                memory = i['memory'],
+                other_connectivity = i['other_connectivity'],
+                hard_drive = i['hard_drive'],
+                usb_ports = i['usb_ports'],
+                video_card = i['video_card'],
+                removable_media = i['removable_media'],
+                physical_address = i['physical_address'],
+                purchase_price = i['purchase_price'],
+                purchase_info = i['purchase_info']
+            )
+            equipment_objects.append(equipment_object)
+        Equipment.objects.bulk_create(equipment_objects)
     print("after equipment")
 
     with open('clientlist.txt') as clientfile:
         client = json.load(clientfile)
+        client_objects = []
         for c in client:
-            add_client(c['name'], c['bpn'], c['note'])
-    clientfile.close()
+            client_object = Client(name = c['name'], bpn = c['bpn'], note = c['note'])
+            client_objects.append(client_object)
+        Client.objects.bulk_create(client_objects)
     print("after client")
 
     with open('checkoutlist.txt') as checkoutfile:
         checkout = json.load(checkoutfile)
+        checkoutfile_objects = []
         for check in checkout:
-            add_checkout(check['client'], check['timestamp'], check['location'], check['equipment'])
-    checkoutfile.close()
+            # TODO fetch all of these at once and remove duplicates
+            checkout_location = Location.objects.filter(building = check['location'][0], room = check['location'][1])[0]
+            checkout_object = Checkout(
+                # TODO fetch all at once
+                client = Client.objects.get(name=check['client']),
+                timestamp = check['timestamp'],
+                location = checkout_location,
+                # TODO fetch all at once
+                equipment = Equipment.objects.get(serial_no = check['equipment'])
+            )
+            checkoutfile_objects.append(checkout_object)
+        Checkout.objects.bulk_create(checkoutfile_objects)
     print("after checkouts")
-
-def add_equipment_type(equiptype):
-    e = EquipmentType.objects.get_or_create(type_name=equiptype)[0]
-    e.save()
-    return e
-
-def add_equipment(sn, equipment_model, equipment_type, count, manufacturer,
-    service_tag, smsu_tag, cpu, optical_drive, size, memory, other_connectivity,
-    hard_drive, usb_ports, video_card, removable_media, physical_address,
-    purchase_price, purchase_info):
-
-    e = Equipment.objects.get_or_create(serial_no = sn,
-        equipment_model = equipment_model,
-        equipment_type = EquipmentType.objects.get(type_name=equipment_type),
-        count = count,
-        manufacturer = manufacturer,
-        service_tag = service_tag,
-        smsu_tag = smsu_tag,
-        cpu = cpu,
-        optical_drive = optical_drive,
-        size = size,
-        memory = memory,
-        other_connectivity = other_connectivity,
-        hard_drive = hard_drive,
-        usb_ports = usb_ports,
-        video_card = video_card,
-        removable_media = removable_media,
-        physical_address = physical_address,
-        purchase_price = purchase_price,
-        purchase_info = purchase_info)[0]
-    e.save()
-    return e
-
-def add_location(building, room):
-    L = Location.objects.get_or_create(building = building, room = room)[0]
-    L.save()
-    return L
-
-def add_client(name, bpn, note):
-    c = Client.objects.get_or_create(name = name, bpn = bpn, note = note)[0]
-    c.save()
-    return c
-
-def add_checkout(client, timestamp, location, equipment):
-    c = Checkout.objects.get_or_create(
-        client = Client.objects.get(name = client),
-        timestamp = timestamp,
-        location = Location.objects.get(building = location[0], room = location[1]),
-        equipment = Equipment.objects.get(serial_no = equipment))[0]
-    c.save()
-    return c
 
 if __name__ =='__main__':
     p = cProfile.Profile()
