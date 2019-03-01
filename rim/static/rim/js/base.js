c113751a0824b63c10b65b3aa6d4ab0d2a4d58fd
Patrick Ayres <theend7321@gmail.com>
2019-02-28 18:58:55 -0600

    Fixed multiple fatal errors, for both the code, and my sanity

--- rim/static/rim/js/base.js
+++ rim/static/rim/js/base.js
@@ -5,10 +5,19 @@ $(document).ready(function() {
         //Sets the expire_date to be a year from now
         expire_date.setTime(expire_date.getTime() + (365*24*60*60*1000))
         document.cookie = "paginate=" + per_page +";expires=" + expire_date.toUTCString() +";path=/";
-        window.location.reload();
     }
 
-    $('#page_submit').click(function() {
+    $('#paginate_form').submit(function(e) {
+        e.preventDefault();
         set_page_cookie($('#page_input').val());
+        var searchParam = new URLSearchParams(window.location.search);
+        if (searchParam.has('page')) {
+            searchParam.delete('page');
+            window.location.search = '?' + searchParam.toString();
+        }
+        else {
+            window.location.reload();
+        }
     })
+
 });
