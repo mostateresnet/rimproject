89aecc3981416e11dea066d7979d6d2a46b36d19
hurt1030 <hurt1030@live.missouristate.edu>
2020-03-11 16:56:40 -0500

    added login and logout capabilities

--- rimproject/settings.py
+++ rimproject/settings.py
@@ -17,6 +17,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 STATIC_DIR = os.path.join(BASE_DIR, 'static')
 
+# Redirect URLs for logging in and logging out
+LOGIN_REDIRECT_URL = "/"
+LOGOUT_REDIRECT_URL = "/"
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
@@ -57,7 +61,7 @@ ROOT_URLCONF = 'rimproject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -121,3 +125,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
--- rimproject/urls.py
+++ rimproject/urls.py
@@ -3,5 +3,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('rim.urls')),
+    path('', include('rim.urls'), name='home'),
+    path('accounts/', include('django.contrib.auth.urls'))
 ]
