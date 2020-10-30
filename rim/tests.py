7b20977fbf6277d07e54e9f49d59b5c3d74fbbd8
sr0409 <sr0409@live.missouristate.edu>
2020-10-30 16:56:35 -0500

    Working on Add

--- rim/tests.py
+++ rim/tests.py
@@ -10,14 +10,19 @@ class Setup(TestCase):
     fixtures = ['dummydb.json']
     def setUp(self):
         self.factory = RequestFactory()
-        self.user = User.objects.create_user(
-            username='test', email='', password='test')
-        self.client.login(username='test', password='test')
+        self.user = User.objects.create(
+            username='test')
+        self.user.set_password('test')
+        self.user.save()
+        login = self.client.login(username='test', password='test')
+        self.assertTrue(login) 
+        
 
 class TestHttpResponse(TestCase): 
     # test_http_response iterates through the pages and checks if all pages all returning HTTP 200(OK) 
     # response. If the test doesn't pass it returns the name, address, and response code of the page that returned it.
     def test_http_response(self):
+        Setup.setUp(self)
         # Url dictionary to hold names and addresses for existing rim web pages, add as neccessary 
         self.urlDictionary = {
             "Home": reverse('home'),
@@ -26,19 +31,19 @@ class TestHttpResponse(TestCase):
             "Client": reverse('client', args = [1]),
             "Client List": reverse('client_list')
         }
-
+        
         for name, url in self.urlDictionary.items():
             response = self.client.get(url, follow=True)
-            self.assertEqual(response.status_code, 200, 'The %s page at %s returned %d (expected 200)' % (name, url, response.status_code))
+            self.assertEqual(response.status_code, 200, 'The %s page at %s returned %d (expected 200) ' % (name, url, response.status_code))
 #End of TestHttpResponse class
         
-class TestAddPage(TestCase):
+# class TestAddPage(TestCase):
+#     fixture = Setup.fixtures
+#     def test_AddForm_valid(self):
+#         add_page = reverse('add')
+#         valid_form_data = {"serial_no":"A999", "hostname":"host123", "equipment_model":"model123","equipment_type":"Laptop","count":"1",
+#                         "manufacturer":"manufact" }
     
-    def test_AddForm_valid(self):
-        add_page = reverse('add')
-        valid_form_data = {"serial_no":"A999", "hostname":"host123", "equipment_model":"model123","equipment_type":"Laptop","count":"1",
-                        "manufacturer":"manufact" }
-    
-        form = EquipmentForm(data={"serial_no":"A999", "equipment_model":"model123", "equipment_type": 'Laptop'})
-        print(form)
-        #self.assertTrue(form.is_valid())
+#         form = EquipmentForm(data={"serial_no":"A999", "equipment_model":"model123", "equipment_type": '1'})
+#         print(form)
+#         #self.assertTrue(form.is_valid())
