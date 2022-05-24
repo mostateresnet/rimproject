379f02390c52ad884b2e873f2f9a8bce01ceb322
Jayanth M <jay@pop-os.localdomain>
2022-05-24 11:59:34 -0500

    Modified model, migrations. Updated requirements.txt

--- rimapi/serializers.py
+++ rimapi/serializers.py
@@ -6,14 +6,14 @@ from .models import ApiError
 class EquipmentSerializer(serializers.ModelSerializer):
     CPU = serializers.CharField(max_length=64, required=False, source='cpu')
     Displays = serializers.JSONField(required=False, source='displays')
-    GPUs = serializers.JSONField(required=False, source='video_card')
+    GPUs = serializers.JSONField(required=False, source='GPU')
     Hostname = serializers.CharField(max_length=100, required=False, source='hostname')
     Manufacturer = serializers.CharField(max_length=30, required=False, source='manufacturer')
     Model = serializers.CharField(max_length=30, source='equipment_model')
     NICs = serializers.JSONField(required=False, source='network_cards')
     RAM = serializers.CharField(max_length=10, required=False, source='memory')
     Serial = serializers.CharField(max_length=100, source='serial_no')
-    Storage = serializers.JSONField(required=False, source='hard_drive')
+    Storage = serializers.JSONField(required=False, source='storage')
     Users = serializers.JSONField(required=False, source='users_info')
     equipment_type = serializers.PrimaryKeyRelatedField(queryset=EquipmentType.objects.all())
 
